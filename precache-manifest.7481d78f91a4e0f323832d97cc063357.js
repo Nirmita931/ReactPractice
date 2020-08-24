178792689135d7bffed50da756402fc855248313
nirmita <nkc@sevadev.com>
2020-08-24 16:18:58 +0545

    Updates

---
+++ precache-manifest.7481d78f91a4e0f323832d97cc063357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54c6170337e3bcd139f9f0693893dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d6fe49b1fb73acae0d",
+    "url": "/static/css/main.2762fa82.chunk.css"
+  },
+  {
+    "revision": "3b1762a930ce61b5f441",
+    "url": "/static/js/2.33b481ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33b481ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d6fe49b1fb73acae0d",
+    "url": "/static/js/main.e01ea4d0.chunk.js"
+  },
+  {
+    "revision": "cb0dc32c741f899db266",
+    "url": "/static/js/runtime-main.40220c3e.js"
+  }
+]);
